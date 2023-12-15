ab63561fe64561d075b2aa5d31e21a2a74629c70
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-15 15:51:01 +0530

    updated the latest code

--- variables.tf
+++ variables.tf
@@ -2,19 +2,8 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-/*variable "cidr_vpc" {
-  description = "CIDR block for the VPC"
-  default     = "10.1.0.0/16"
-}
-variable "cidr_subnet" {
-  description = "CIDR block for the subnet"
-  default     = "10.1.0.0/24"
-}*/
 
 variable "environment_tag" {
-  description = "Environment tag"
+  description = "Tokyo tag"
   default     = "Learn"
 }
