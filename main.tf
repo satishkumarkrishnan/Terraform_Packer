0087c69d38c14138880cfc1789fbe3df9c304259
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-15 11:14:49 +0530

    TF packer code

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.42.0"
+      version = "5.2.0"
     }
   }
-  required_version = ">= 0.14.5"
 }
 
 provider "aws" {
