235bde907d85015ce73763d4df53fb4fac1d90fb
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-18 16:10:00 +0530

    added VPC module

--- main.tf
+++ main.tf
@@ -14,77 +14,10 @@ module "vpc" {
 }
 
 
-/*resource "aws_vpc" "vpc" {
-  cidr_block           = module.vpc.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  #vpc_id = aws_vpc.vpc.id
-  vpc_id = module.vpc.vpc_id
-  
-}
-
-resource "aws_subnet" "subnet_public" {
-  vpc_id     = module.vpc.vpc_id
-  #cidr_block = module.vpc.vpc_fe_subnet.id
-  cidr_block = "10.1.0.0/24"
-  
-}
-
-resource "aws_route_table" "rtb_public" {
-  vpc_id = module.vpc.vpc_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = module.vpc.vpc_ig
-  }
-}
-
-resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = module.vpc.vpc_fe_subnet.id
-  route_table_id = module.vpc.vpc_rt
-}
-
-resource "aws_security_group" "sg_22_80" {
-  name   = "sg_22"
-  vpc_id = module.vpc.vpc_id
-
-  # SSH access from the VPC
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}*/
 
 resource "aws_instance" "web" {
   ami                         = "ami-0c20d1e87e986f2cc"
   instance_type               = "t2.micro"
-  #subnet_id                   = aws_subnet.subnet_public.id  
   subnet_id                   = module.vpc.vpc_fe_subnet.id  
   vpc_security_group_ids      = [module.vpc.vpc_fe_sg]
   associate_public_ip_address = true
